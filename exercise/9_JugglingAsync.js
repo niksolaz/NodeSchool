69f0c0579782d46233d668d951c9f0e09b84ff07
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-25 17:44:18 +0000

    Juggling Async

--- exercise/9_JugglingAsync.js
+++ exercise/9_JugglingAsync.js
@@ -1,15 +1,53 @@
-var http = require("http");
-var bl = require("bl");
+var http = require('http');
+var urls = [process.argv[2], process.argv[3], process.argv[4]];
 
+function printResponseData(urls, n){
+    if (n > urls.length-1){
+        return;
+    }
+    http.get(urls[n], function(res) {
+      res.setEncoding('utf8');
+      var body = '';
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+            console.log(body.toString());
+            printResponseData(urls, n+1);
+      });
+    }); 
+}
+printResponseData(urls, 0);
 
-var Data = process.argv[2];
+/*
+Solutions
+________________________________________________________________________
+    var bl = require('bl')    
+    var results = []
+    var count = 0
+    
+    function printResults () {
+      for (var i = 0; i < 3; i++)
+        console.log(results[i])
+    }
+    
+    function httpGet (index) {
+      http.get(process.argv[2 + index], function (response) {
+        response.pipe(bl(function (err, data) {
+          if (err)
+            return console.error(err)
+    
+          results[index] = data.toString()
+          count++
+    
+          if (count == 3)
+            printResults()
+        }))
+      })
+    }
+    
+    for (var i = 0; i < 3; i++)
+      httpGet(i)
 
-
-http.get(Data,function(response){
-    response.setEncoding('utf8');
-    response.pipe(bl(function(err,data){
-        if(err) throw err;
-        console.log(data);
-        
-    }));
-});
+────────────────────────────────────────────────────────────────────────────────
+*/
