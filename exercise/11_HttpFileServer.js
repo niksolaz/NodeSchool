f170def5d03cf527236e9f8abeb7be4fe6e66ff7
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-26 13:19:22 +0000

    test solved http file server in learnyounode

--- exercise/11_HttpFileServer.js
+++ exercise/11_HttpFileServer.js
@@ -3,20 +3,32 @@ var fs = require("fs");
 
 
 var port = process.argv[2];
+var file =process.argv[3];
 
 var server = http.createServer(function (req,res){
     console.log('Connected');
-    var file =process.argv[3];
-    console.log('file: ',file);
-    /*
-    var read = fs.createReadStream(file,function(response){
-                read.pipe(function(err,data){
-                    if(err) return console.error('Error: ',err.message);
-                    read.on('open',console.log);
-                });
-    });
-    */
+    
     var read = fs.createReadStream(file);
     read.pipe(res);
 })
 server.listen(Number(port));
+
+//Test Solved!
+
+/*
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    var http = require('http')
+    var fs = require('fs')
+    
+    var server = http.createServer(function (req, res) {
+      res.writeHead(200, { 'content-type': 'text/plain' })
+    
+      fs.createReadStream(process.argv[3]).pipe(res)
+    })
+    
+    server.listen(Number(process.argv[2]))
+
+────────────────────────────────────────────────────────────────────────────────
+*/
