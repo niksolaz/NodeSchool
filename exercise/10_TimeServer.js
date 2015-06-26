fcd20edaeb497c7d7ca92ecd92eb765edd6f53da
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-26 09:08:01 +0000

    date format added

--- exercise/10_TimeServer.js
+++ exercise/10_TimeServer.js
@@ -1,11 +1,19 @@
 var net = require("net");
-
+//var strftime = require("strttime")
 var portNumber = process.argv[2];
 
+var date = new Date()
+
+var Y = date.getFullYear('YYYY');
+var m = date.getMonth('mm');
+var d = date.getDate('dd');
+var H = date.getHours('HH');
+var M = date.getMinutes('MM');
+
 var server = net.createServer(function(socket){ // connection listener
-    console.log('client connected');
+    console.log('%d-%d-%d %d:%d',Y,m,d,H,M);
     //socket handling logic
-    socket.write('Echo server\r\n');
+    socket.write(date);
     socket.end()
 });
 server.listen(portNumber);
