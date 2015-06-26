6817286e1b498dd050f869b8c91d5ae0fafcb0d9
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-26 10:05:07 +0000

    solution exercise time server

--- exercise/10_TimeServer.js
+++ exercise/10_TimeServer.js
@@ -2,18 +2,48 @@ var net = require("net");
 var strftime = require("strftime")
 var portNumber = process.argv[2];
 
-var date = new Date()
+function now(x) { return x < 10 ? '0' + x:x}
 
-var Y = date.getFullYear(strftime('%Y'));
-var m = date.getMonth(strftime('%m'));
-var d = date.getDate(strftime('%d'));
-var H = date.getHours(strftime('%H'));
-var M = date.getMinutes(strftime('M'));
+var date = new Date()
+var nowDate = date.getFullYear(strftime('%Y')) + '-'
+              + now(date.getMonth(strftime('%m'))+ 1) + '-'
+              + now(date.getDate(strftime('%d'))) + ' '
+              + now(date.getHours(strftime('%H'))) + ':'
+              + now(date.getMinutes(strftime('M'))) + '\n';
 
 var server = net.createServer(function(socket){ // connection listener
-    console.log('%d-%d-%d %d:%d',Y,m,d,H,M);
+   
     //socket handling logic
-    socket.write(date);
-    socket.end()
+    socket.write('',date);
+    socket.end(nowDate)
 });
 server.listen(portNumber);
+
+// Test Solved!
+
+/*
+OFFICIAL SOLUTION
+────────────────────────────────────────────────────────────────────────────────
+    var net = require('net')
+    
+    function zeroFill(i) {
+      return (i < 10 ? '0' : '') + i
+    }
+    
+    function now () {
+      var d = new Date()
+      return d.getFullYear() + '-'
+        + zeroFill(d.getMonth() + 1) + '-'
+        + zeroFill(d.getDate()) + ' '
+        + zeroFill(d.getHours()) + ':'
+        + zeroFill(d.getMinutes())
+    }
+    
+    var server = net.createServer(function (socket) {
+      socket.end(now() + '\n')
+    })
+    
+    server.listen(Number(process.argv[2]))
+
+────────────────────────────────────────────────────────────────────────────────
+*/
