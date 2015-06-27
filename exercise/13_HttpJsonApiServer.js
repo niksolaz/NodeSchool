38708c8b7322f5b08119d4b7864173ec0ffba2d9
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-27 13:44:35 +0000

    Added element of times by unixtime

--- exercise/13_HttpJsonApiServer.js
+++ exercise/13_HttpJsonApiServer.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var url = require("url");
+var strftime = require("strftime");
 
 var port = process.argv[2];
 
@@ -17,5 +18,11 @@ function unixTime(time){
 
 var server = http.createServer(function(req,res){
     console.log('Connected');
+    var path = url.parse(req.url,true);
+    var date = new Date();
+    var nowJsonTime = jsonTime(date.getHours(strftime('%H'))) + ':'
+                      jsonTime(date.getMinutes(strftime('%M'))) + ':'
+                      jsonTime(date.getSeconds(strftime('%S'))) + '\n';
+    var nowUnixTime = unixTime(date.getTime(strftime('%L')));
 });
 server.listen(Number(port));
