ffd141e4acc671d70ef090a8b11123fbfbccf47a
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-27 13:14:39 +0000

    Added basic server with createServer

--- exercise/13_HttpJsonApiServer.js
+++ exercise/13_HttpJsonApiServer.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var url = require("url");
 
+var port = process.argv[2];
+
 function jsonTime(time){
     return {
         hour: time.getHours(),
@@ -12,3 +14,8 @@ function jsonTime(time){
 function unixTime(time){
     return { unixtime: time.getTime()}
 }
+
+var server = http.createServer(req,res){
+    
+}
+server.listen(Number(port));
