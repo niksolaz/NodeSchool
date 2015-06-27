74a9b8069c50ad6c76c73244d4b8eacea906939c
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-27 15:12:39 +0000

    try function

--- exercise/13_HttpJsonApiServer.js
+++ exercise/13_HttpJsonApiServer.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var url = require("url");
-var strftime = require("strftime");
+
 
 var port = process.argv[2];
 
@@ -19,11 +19,7 @@ function unixTime(time){
 var server = http.createServer(function(req,res){
     console.log('Connected');
     var path = url.parse(req.url,true);
-    var date = new Date(path.query.iso);
-    /*var nowJsonTime = jsonTime(date.getHours(strftime('%H'))) + ':'
-                      jsonTime(date.getMinutes(strftime('%M'))) + ':'
-                      jsonTime(date.getSeconds(strftime('%S'))) + '\n';
-    var nowUnixTime = unixTime(date.getTime(strftime('%L')));*/
+    var date = new Date(path.query.iso).toISOString();
     var result;
     
     if(/^\/api\/parsetime/.test(path) ){
@@ -34,9 +30,11 @@ var server = http.createServer(function(req,res){
     if (result) {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify(result))
+        
     } else {
         res.writeHead(404)
         res.end()
+       
     }
 });
 server.listen(Number(port));
