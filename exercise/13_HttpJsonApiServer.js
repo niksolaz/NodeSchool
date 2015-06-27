c9e434727711bcb8ebca260762be910ec3d7c09d
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-27 14:42:13 +0000

    Added new variable

--- exercise/13_HttpJsonApiServer.js
+++ exercise/13_HttpJsonApiServer.js
@@ -19,15 +19,24 @@ function unixTime(time){
 var server = http.createServer(function(req,res){
     console.log('Connected');
     var path = url.parse(req.url,true);
-    var date = new Date();
-    var nowJsonTime = jsonTime(date.getHours(strftime('%H'))) + ':'
+    var date = new Date(path.query.iso);
+    /*var nowJsonTime = jsonTime(date.getHours(strftime('%H'))) + ':'
                       jsonTime(date.getMinutes(strftime('%M'))) + ':'
                       jsonTime(date.getSeconds(strftime('%S'))) + '\n';
-    var nowUnixTime = unixTime(date.getTime(strftime('%L')));
+    var nowUnixTime = unixTime(date.getTime(strftime('%L')));*/
+    var result;
+    
     if(/^\/api\/parsetime/.test(path) ){
         return jsonTime(date);
     }else if(/^\/api\/unixtime/.test(path)){
         return unixTime(date);
+    }
+    if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(result))
+    } else {
+        res.writeHead(404)
+        res.end()
     }
 });
 server.listen(Number(port));
