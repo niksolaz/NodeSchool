ac49c06b70ecbbcfaaaf2ef102d7ff4dc0f438fc
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-29 11:42:25 +0000

    Comment exercise 2

--- exercise/2_BabySteps.js
+++ exercise/2_BabySteps.js
@@ -1,9 +1,10 @@
+// Create variable 
 var sum = 0;
-
+// Applied FOR. The process object is a global object and can be accessed from anywhere.
 for(var i=2; i<process.argv.length; i++){
-    
-    sum += Number(process.argv[i]);
+ //do something! Proccess is an array containing the command line arguments.   
+    sum += Number(process.argv[i]); 
 }
-console.log(sum);
+console.log(sum); // Printed variable
 
 //Test Passed!
