ee451bbb8624f13dab4a603c4268a2fddc518c3f
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-29 11:49:55 +0000

    Comment exercise 4

--- exercise/4_MyFirstAsyncIO.js
+++ exercise/4_MyFirstAsyncIO.js
@@ -1,8 +1,9 @@
-var fs = require("fs");
-var file = process.argv[2];
+var fs = require("fs"); // Module fs
+var file = process.argv[2]; // Proccess is an array containing the command line arguments.
 
-fs.readFile(file,{encoding:'utf8'},function(err,data){
-    if(err) throw err;
+//Asynchronously reads the entire contents of a file
+fs.readFile(file,{encoding:'utf8'},function(err,data){ 
+    if(err) throw err; 
     console.log(data.toString().split('\n').length -1);
 });
 
