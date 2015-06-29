b2615b82b0113a06e64054f320fe65fc6c3fd76e
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-29 12:05:41 +0000

    Comment exercise 5

--- exercise/5_FilteredLS.js
+++ exercise/5_FilteredLS.js
@@ -1,16 +1,21 @@
-var fs = require("fs");
+var fs = require("fs"); //Module fs ... Accessing the File System in Node.Js
+var path = require("path");// Module path. This module contains utilities for handling and transforming file paths.
 
-var path = require("path");
-var File = process.argv[2];
+var File = process.argv[2];// Proccess is an array containing the command line arguments
+/*The first element will be 'node', 
+the second element will be the name of the JavaScript file. 
+The next elements will be any additional command line argument.
+PROCESS.ARGV[NODE,FILE,ELEMENT,...ELEMENT..]*/
 var ext = '.' + process.argv[3];
 
-fs.readdir(File,function(err,data){
+fs.readdir(File,function(err,data){ //Reads the contents of a directory. Asynchronous.
     if(err){
         throw err
     }
     data.forEach(function(file){
+    //Return the extension of the path, from the last '.' to end of string in the last portion of the path.    
         var extEnd = path.extname(file);
-        if(extEnd === ext){
+        if(extEnd === ext){ // Condition
             console.log(file);
         }
     });
