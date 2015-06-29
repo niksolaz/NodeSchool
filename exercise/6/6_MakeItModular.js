6a79c52a915c184b79c06c27dc26b71477edb1cf
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-29 12:12:09 +0000

    Comment exercise 6

--- exercise/6/6_MakeItModular.js
+++ exercise/6/6_MakeItModular.js
@@ -1,5 +1,9 @@
-var filterFn = require('./mymodule.js')
-var dir = process.argv[2]
+var filterFn = require('./mymodule.js') // Call  own module
+var dir = process.argv[2] // // Proccess is an array containing the command line arguments
+/*The first element will be 'node', 
+the second element will be the name of the JavaScript file. 
+The next elements will be any additional command line argument.
+PROCESS.ARGV[NODE,FILE,ELEMENT,...ELEMENT..]*/
 var filterStr = process.argv[3]
 
 filterFn(dir, filterStr, function (err, list) {
