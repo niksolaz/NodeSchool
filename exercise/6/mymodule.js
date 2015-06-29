dad1af907ecf04eed47b518c02217543a029be72
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-29 12:10:22 +0000

    Comment exercise 6 module

--- exercise/6/mymodule.js
+++ exercise/6/mymodule.js
@@ -1,13 +1,15 @@
+
 var fs = require('fs')
 var path = require('path')
-
+//Creating module 
 module.exports = function (dir, filterStr, callback) {
 
-  fs.readdir(dir, function (err, list) {
+  fs.readdir(dir, function (err, list) {//Reads the contents of a directory. Asynchronous.
     if (err)
       return callback(err)
-
+//The filter() method creates a new array with all elements that pass the test implemented by the provided function.
     list = list.filter(function (file) {
+//Return the extension of the path, from the last '.' to end of string in the last portion of the path.      
       return path.extname(file) === '.' + filterStr
     })
 
