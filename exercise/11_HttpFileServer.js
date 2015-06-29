386a47efc59f4353c28fedf6e0331f771427fbd5
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-06-29 13:26:45 +0000

    Comment exercise 11

--- exercise/11_HttpFileServer.js
+++ exercise/11_HttpFileServer.js
@@ -1,17 +1,17 @@
-var http = require("http");
-var fs = require("fs");
+var http = require("http");// Module http
+var fs = require("fs"); //Module fs ... Accessing the File System in Node.Js
 
 
-var port = process.argv[2];
+var port = process.argv[2];// Proccess is an array containing the command line arguments.
 var file =process.argv[3];
 
-var server = http.createServer(function (req,res){
+var server = http.createServer(function (req,res){// Create server
     console.log('Connected');
     
-    var read = fs.createReadStream(file);
-    read.pipe(res);
+    var read = fs.createReadStream(file);//Returns a new ReadStream object (See Readable Stream).
+    read.pipe(res);////Pipe reads the data source and writes the destination without managing the stream alone.
 })
-server.listen(Number(port));
+server.listen(Number(port));//Port of the  server 
 
 //Test Solved!
 
