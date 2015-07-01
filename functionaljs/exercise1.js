8e563dc030113f584647353f4eee10e7b71aa34f
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-07-01 15:50:15 +0000

    Exercise1:Write a function that takes an input string and returns it uppercased.

---
+++ functionaljs/exercise1.js
@@ -0,0 +1,5 @@
+"use strict"
+
+module.exports =  function (input){//input: a String of random words (lorem ipsum).
+    return input.toUpperCase();  //return a string in upper case from (lorem ipsum)
+}
